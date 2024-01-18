4979b9d0e25ffd796cd8cb4d8bb43feea7b13c47
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-18 19:19:11 +0000

    Test 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,3 @@
----SQL script that creates a table users
---- SQL script that creates a table users
 --- SQL script that creates a table users
 CREATE TABLE IF NOT EXISTS users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
