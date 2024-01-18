dbea5fb14c21b9f21ee3731af9a0faeeeb02f26b
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-18 22:43:08 +0000

    Task 5-valid_email.sql

--- 0x00-MySQL_Advanced/5-valid_email.sql
+++ 0x00-MySQL_Advanced/5-valid_email.sql
@@ -1,1 +1,11 @@
 -- 
+DELIMITER $$
+CREATE TRIGGER validate_email
+BEFORE UPDATE ON users
+FOR EACH ROW
+BEGIN
+    IF OLD.email != NEW.email THEN
+        SET NEW.valid_email = 0;
+    END IF;
+END $$
+DELIMITER ;
