085669aeda3e63a3ae52fcfd0fc732d67f406526
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-18 15:43:46 +0000

    Task 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,1 +1,8 @@
--- 
+-- SQL script that creates a table users
+CREATE TABLE IF NOT EXISTS users (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+)
+
