14a216c59145f8b0b8e5fb64d8b4a9e439dda2a0
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-18 19:16:56 +0000

    Test 0-uniq_users.sql

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,5 +1,5 @@
--- SQL script that ranks country origins of bands,
--- ordered by the number of (non-unique) fans
+--- SQL script that ranks country origins of bands,
+--- ordered by the number of (non-unique) fans
 SELECT origin, SUM(fans) AS nb_fans
 FROM metal_bands
 GROUP BY origin
