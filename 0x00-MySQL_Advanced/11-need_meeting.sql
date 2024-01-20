803e7af995d2f1df524aaaa5998006f2b065e7f5
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-20 04:15:08 +0000

    Fix Task 11-need_meeting.sql

--- 0x00-MySQL_Advanced/11-need_meeting.sql
+++ 0x00-MySQL_Advanced/11-need_meeting.sql
@@ -3,4 +3,4 @@
 CREATE VIEW need_meeting AS
 SELECT name FROM students
 WHERE score < 80 &&
-(last_meeting = NULL || DATEDIFF(CURDATE(), last_meeting) > 30);
+(last_meeting IS NULL || DATEDIFF(CURDATE(), last_meeting) > 30);
