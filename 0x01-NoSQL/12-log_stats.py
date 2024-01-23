5628688423bad8c8799ca28fcade198f90fb1d91
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-23 03:11:58 +0000

    Fix Task 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -8,24 +8,18 @@ from pymongo import MongoClient
 def provides_stats():
     """ Provides Stats Function """
     client = MongoClient('mongodb://127.0.0.1:27017')
-
     db = client.logs.nginx
-    x = db.count_documents({})
-    get = db.count_documents({'method': 'GET'})
-    post = db.count_documents({'method': 'POST'})
-    put = db.count_documents({'method': 'PUT'})
-    patch = db.count_documents({'method': 'PATCH'})
-    delete = db.count_documents({'method': 'DELETE'})
+
+    print(f"{db.count_documents({})} logs")
+    print("Methods:")
+
+    methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
+
+    for method in methods:
+        print(f"\tmethod {method}: {db.count_documents({'method': method})}")
+
     status_get = db.count_documents({'method': 'GET', 'path': '/status'})
-    data = f"""{x} logs
-Methods:
-    method GET: {get}
-    method POST: {post}
-    method PUT: {put}
-    method PATCH: {patch}
-    method DELETE: {delete}
-{status_get} status check"""
-    print(data)
+    print(f"{status_get} status check")
 
 
 if __name__ == "__main__":
