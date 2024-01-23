fee536b06e62855f3fd8df752d7fdcf915d03e78
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-23 02:10:06 +0000

    Task 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-import pymongo
+""" Changes all topics of a school document based on the name """
 
+
+def update_topics(mongo_collection, name, topics):
+    """ Update Topics Function """
+    try:
+        mongo_collection.update_one({"name": name},
+                                    {"$set": {"topics": topics}})
+    except Exception as ex:
+        print(str(ex))
