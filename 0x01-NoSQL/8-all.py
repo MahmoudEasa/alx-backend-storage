e34f5a6fc9e2549344b91cabc50711b7a699b611
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-23 01:41:54 +0000

    Task 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3
-import pymongo
+""" lists all documents in a collection """
+from pymongo import MongoClient
 
+
+def list_all(mongo_collection):
+    """ List All Function """
+    db = mongo_collection
+    return (db.find())
