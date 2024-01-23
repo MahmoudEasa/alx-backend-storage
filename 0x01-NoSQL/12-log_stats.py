5a662994ce950e1508b8cbb1c54a6ecc79ef9e48
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-23 02:57:19 +0000

    Task 12-log_stats.py

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -1,3 +1,31 @@
 #!/usr/bin/env python3
-import pymongo
+""" Python script that provides some stats
+    about Nginx logs stored in MongoDB
+"""
+from pymongo import MongoClient
 
+
+def provides_stats():
+    """ Provides Stats Function """
+    client = MongoClient('mongodb://127.0.0.1:27017')
+    db = client.logs.nginx
+    x = db.estimated_document_count()
+    get = db.count_documents({'method': 'GET'})
+    post = db.count_documents({'method': 'POST'})
+    put = db.count_documents({'method': 'PUT'})
+    patch = db.count_documents({'method': 'PATCH'})
+    delete = db.count_documents({'method': 'DELETE'})
+    status_get = db.count_documents({'method': 'GET', 'path': '/status'})
+    data = f"""{x} logs
+Methods:
+    method GET: {get}
+    method POST: {post}
+    method PUT: {put}
+    method PATCH: {patch}
+    method DELETE: {delete}
+{status_get} status check"""
+    print(data)
+
+
+if __name__ == "__main__":
+    provides_stats()
