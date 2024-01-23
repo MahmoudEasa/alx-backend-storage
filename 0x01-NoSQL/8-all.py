b229cd466618506831d020b912c07f04cbe5012c
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-23 01:52:37 +0000

    Task 9-insert_school.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
 """ lists all documents in a collection """
-from pymongo import MongoClient
 
 
 def list_all(mongo_collection):
     """ List All Function """
-    db = mongo_collection
-    return (db.find())
+    return (mongo_collection.find())
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
-import pymongo
+""" Inserts a new document in a collection based on kwargs """
 
+
+def insert_school(mongo_collection, **kwargs):
+    """ Insert School Function """
+    try:
+        result = mongo_collection.insert_one(kwargs)
+        return (result.inserted_id)
+    except Exception as ex:
+        print(str(ex))
