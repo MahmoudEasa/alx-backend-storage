eeba62878ea0ca0ae99e05af10f4a4127df52b41
MahmoudEasa <MahmoudEasa@gmail.com>
2024-01-24 22:00:43 +0000

    Task exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -1,1 +1,22 @@
 #!/usr/bin/env python3
+""" Create a Cache class. In the __init__ method,
+    store an instance of the Redis client as a private
+    variable named _redis (using redis.Redis())
+    and flush the instance using flushdb
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ Cache class """
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        """ Store Function """
+        random_key = str(uuid.uuid4())
+        self._redis.set(random_key, data)
+        return (random_key)
